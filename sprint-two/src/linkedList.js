2b09b0e1f0407aab9563e8e31eec3cf22cdcffaf
PRIGOOSE <PRIYALGHOSE@GMAIL.COM>
2018-03-08 11:25:34 -0800

    -Before lecture

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,12 +1,19 @@
 var LinkedList = function() {
+  //debugger;
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    this.tail = node;
+    if (this.head === null) {
+      this.head = node;
+    }
   };
 
   list.removeHead = function() {
+    
   };
 
   list.contains = function(target) {
@@ -22,8 +29,10 @@ var Node = function(value) {
   node.next = null;
 
   return node;
+  //connect them?
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//console.log(tail.value)
