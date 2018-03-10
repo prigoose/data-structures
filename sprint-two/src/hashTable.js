6c5ceedd038627b300cb117b28ee7a96c1844594
PRIGOOSE <PRIYALGHOSE@GMAIL.COM>
2018-03-10 10:37:23 -0800

    -Passed all Hash Tables tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,41 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) === undefined) { 
+    this._storage.set(index, [[k, v]]);
+  } else if (this._storage.get(index) !== undefined) {
+    var value = this._storage.get(index);
+    var isOld = false;
+    for (var i = 0; i < value.length; i++) {
+        if (value[i][0] === k) {
+          value[i][1] = v;
+          isOld = true;
+        } 
+    }
+    if (isOld === false) {
+      value.push([k, v]);
+    }
+  } 
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var value = this._storage.get(index);
+  for (var i = 0; i < value.length; i++) {
+    if (value[i][0] === k) {
+      return value[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var value = this._storage.get(index);
+  for (var i = 0; i < value.length; i++) {
+    if (value[i][0] === k) {
+      value.splice(i, 1);
+    }
+  }
 };
 
 
@@ -22,5 +49,6 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// It's constant time because the hashing function points us to exactly the index we need.
 
 
